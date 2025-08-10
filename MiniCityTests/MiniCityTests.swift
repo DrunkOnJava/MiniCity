5156075b003d6fe8eafea3151470b0cce20f816b
drunkonjava <griffinradcliffe@gmail.com>
2025-08-09 23:04:15 -0400

    Initial Commit

---
+++ MiniCityTests/MiniCityTests.swift
@@ -0,0 +1,17 @@
+//
+//  MiniCityTests.swift
+//  MiniCityTests
+//
+//  Created by Griffin on 8/9/25.
+//
+
+import Testing
+@testable import MiniCity
+
+struct MiniCityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
